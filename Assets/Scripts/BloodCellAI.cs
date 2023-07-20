4f6247bfbaa4f91111b2042604a777315108f299
Ava <PercentMage09@gmail.com>
2023-07-20 21:27:18 +0100

    Created white blood cell prefab

--- Assets/Scripts/BloodCellAI.cs
+++ Assets/Scripts/BloodCellAI.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class BloodCellAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody body;
+    public float timeToWait;
+    public float maxForce;
+
+    public void Start()
     {
-        
+        StartCoroutine(waiter());
     }
 
-    // Update is called once per frame
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(timeToWait);
+    }
+
     void Update()
     {
-        
+        waiter();
+        float direction = Random.Range(0,2);
+        float appliedForce = Random.Range(-maxForce,maxForce);
+
+        if (direction == 0)
+        {
+            body.velocity = new Vector3(appliedForce,0,0);
+        }
+        else if(direction == 1)
+        {
+            body.velocity = new Vector3(0,appliedForce,0);
+        }
+        else if(direction == 1)
+        {
+            body.velocity = new Vector3(0, 0, appliedForce);
+        }
     }
 }
